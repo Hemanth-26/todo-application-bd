5ce9d04f79aa9b0b5913a5cc743c1c0d4c3202e7
Hemanth <hemanth@artifintel.in>
2023-03-05 22:26:49 +0530

    Changed the encrypt password for mongodb and try to fix database issue

--- database/conn.js
+++ database/conn.js
@@ -4,7 +4,7 @@ async function connect () {
     // const userNameMongodb = encodeURIComponent(process.env.MONGODB_USERNAME);
     // const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
     const userNameMongodb = encodeURIComponent("hemanth26");
-    const password = encodeURIComponent("Hemanth@260820");
+    const password = encodeURIComponent("iRAY0cTkXr4o5s08");
 
     // const mongoUrl = "mongodb://127.0.0.1:27017/";
     const mongoUrl = `mongodb+srv://${userNameMongodb}:${password}@cluster0.icoqamg.mongodb.net/?retryWrites=true&w=majority`;
