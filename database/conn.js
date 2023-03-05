2555d38ea31ae27f2b8006cb6b8ba53081136f09
Hemanth <hemanth@artifintel.in>
2023-03-05 21:49:26 +0530

    Fixing database connection

--- database/conn.js
+++ database/conn.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 async function connect () {
-    const userNameMongodb = encodeURIComponent(process.env.MONGODB_USERNAME);
-    const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
-    // console.log(password);
+    // const userNameMongodb = encodeURIComponent(process.env.MONGODB_USERNAME);
+    // const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+    const userNameMongodb = encodeURIComponent("hemanth26");
+    const password = encodeURIComponent("Hemanth@260820");
 
     // const mongoUrl = "mongodb://127.0.0.1:27017/";
     const mongoUrl = `mongodb+srv://${userNameMongodb}:${password}@cluster0.icoqamg.mongodb.net/?retryWrites=true&w=majority`;
