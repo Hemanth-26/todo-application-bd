37db763b6be84e7defbe9ca5cfffbd5ba189e479
Hemanth <hemanth@artifintel.in>
2023-03-07 13:43:51 +0530

    Changed the mongodb connection

--- database/conn.js
+++ database/conn.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 async function connect () {
-    // const userNameMongodb = encodeURIComponent(process.env.MONGODB_USERNAME);
-    // const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
-    const userNameMongodb = encodeURIComponent("hemanth26");
-    const password = encodeURIComponent("iRAY0cTkXr4o5s08");
+    const userNameMongodb = encodeURIComponent(process.env.MONGODB_USERNAME);
+    const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+    // const userNameMongodb = encodeURIComponent("hemanth26");
+    // const password = encodeURIComponent("iRAY0cTkXr4o5s08");
 
     // const mongoUrl = "mongodb://127.0.0.1:27017/";
     const mongoUrl = `mongodb+srv://${userNameMongodb}:${password}@cluster0.icoqamg.mongodb.net/?retryWrites=true&w=majority`;
